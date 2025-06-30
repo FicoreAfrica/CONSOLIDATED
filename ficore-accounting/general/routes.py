10d14f8e433921b884b83123a4413d329910f1b8
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-06-30 21:21:08 +0100

    Update routes.py

--- ficore-accounting/general/routes.py
+++ ficore-accounting/general/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
 from translations import trans
-from flask import session
 from flask_login import current_user
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session
+from flask_login import login_required, current_user
+from translations import trans
+from utils import trans_function, requires_role, check_coin_balance, format_currency, format_date, get_mongo_db, is_admin, get_user_query
+from bson import ObjectId
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, SubmitField
+from wtforms.validators import DataRequired, Optional
+import logging
 
 general_bp = Blueprint('general_bp', __name__, template_folder='templates/general')
 
