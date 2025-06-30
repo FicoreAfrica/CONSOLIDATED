0a71a536b0ae778423f9b195ab6c46054c6f2a45
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-06-30 15:17:40 +0100

    Update app.py

--- ficore-accounting/app.py
+++ ficore-accounting/app.py
@@ -1187,6 +1187,7 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=os.getenv('FLASK_ENV', 'development') == 'development')
