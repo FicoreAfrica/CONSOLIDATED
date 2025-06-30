324836522c4521813ca2860d72b36dc13c7b9da0
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-06-30 21:20:17 +0100

    Update routes.py

--- ficore-accounting/general/routes.py
+++ ficore-accounting/general/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from translations import trans
 from flask import session
+from flask_login import current_user
 
 general_bp = Blueprint('general_bp', __name__, template_folder='templates/general')
 
