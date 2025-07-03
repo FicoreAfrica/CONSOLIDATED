715e20aed425786a5e3c20dd3947afca3bde7ce8
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-07-03 13:31:08 +0100

    Update app.py

--- ficore-accounting/app.py
+++ ficore-accounting/app.py
@@ -44,7 +44,7 @@ load_dotenv()
 
 # Set up logging
 root_logger = logging.getLogger('ficore_app')
-root_logger.setLevel(logging.INFO)
+root_logger.setLevel(logging.WARNING)
 
 class SessionFormatter(logging.Formatter):
     def format(self, record):
