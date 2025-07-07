f59962b898d538b56bd62602ab933fbc5c574edb
FICORE TOOLS SUITE <ficore.ai.africa@gmail.com>
2025-07-07 21:56:06 +0100

    Update financial_health.py

--- ficore-accounting/personal/financial_health.py
+++ ficore-accounting/personal/financial_health.py
@@ -113,7 +113,7 @@ def main():
     form_data = {}
     if current_user.is_authenticated:
         form_data['email'] = current_user.email
-        form_data['first_name'] = current_user.username
+        form_data['first_name'] = current_user.get_first_name()
     
     form = FinancialHealthForm(data=form_data)
     
